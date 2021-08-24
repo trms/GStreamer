a9a7ed1e6358ea09d77a20a9e7b18f94f0e2cbd2
Seungha Yang <seungha@centricular.com>
2021-08-24 20:39:14 +0900

    hls: Add hlswebvttsink element
    Introduce a new hlssink element for producing WebVTT playlist.

---
+++ subprojects/gst-plugins-bad/ext/hls/gsthlswebvttsink.h
@@ -0,0 +1,36 @@
+/* GStreamer
+ * Copyright (C) 2021 Seungha Yang <seungha@centricular.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GST_HLS_WEBVTT_SINK_H__
+#define __GST_HLS_WEBVTT_SINK_H__
+
+#include <gst/gst.h>
+#include <gst/base/gstbasesink.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_HLS_WEBVTT_SINK (gst_hls_webvtt_sink_get_type())
+G_DECLARE_FINAL_TYPE (GstHlsWebvttSink, gst_hls_webvtt_sink,
+    GST, HLS_WEBVTT_SINK, GstBaseSink);
+
+gboolean  gst_hls_webvtt_sink_plugin_init (GstPlugin * plugin);
+
+G_END_DECLS
+
+#endif /* __GST_HLS_WEB_VTT_SINK_H__ */
