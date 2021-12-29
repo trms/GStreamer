f4da37a841ed878ea0a7fae4725cce9e256e7e5c
Seungha Yang <seungha@centricular.com>
2021-12-30 02:18:19 +0900

    filesrc: Port to FILE struct
    ... in order to make use of OS layer file I/O abstraction/optimiztion

--- subprojects/gstreamer/plugins/elements/gstfilesrc.c
+++ subprojects/gstreamer/plugins/elements/gstfilesrc.c
@@ -38,7 +38,6 @@
 #endif
 
 #include <gst/gst.h>
-#include <glib/gstdio.h>
 #include "gstfilesrc.h"
 #include "gstcoreelementselements.h"
 
@@ -52,6 +51,10 @@
  * variants, so explicitly define it that way. */
 #undef lseek
 #define lseek _lseeki64
+#ifndef HAVE_FSEEKO
+/* fseeko is not defined in Windows SDK's stdio.h */
+#define fseeko _fseeki64
+#endif
 #undef off_t
 #define off_t guint64
 /* Prevent stat.h from defining the stat* functions as
@@ -190,12 +193,6 @@ gst_file_src_class_init (GstFileSrcClass * klass)
 static void
 gst_file_src_init (GstFileSrc * src)
 {
-  src->filename = NULL;
-  src->fd = 0;
-  src->uri = NULL;
-
-  src->is_regular = FALSE;
-
   gst_base_src_set_blocksize (GST_BASE_SRC (src), DEFAULT_BLOCKSIZE);
 }
 
@@ -315,7 +312,7 @@ gst_file_src_fill (GstBaseSrc * basesrc, guint64 offset, guint length,
 {
   GstFileSrc *src;
   guint to_read, bytes_read;
-  int ret;
+  gsize ret;
   GstMapInfo info;
   guint8 *data;
 
@@ -324,8 +321,8 @@ gst_file_src_fill (GstBaseSrc * basesrc, guint64 offset, guint length,
   if (G_UNLIKELY (offset != -1 && src->read_position != offset)) {
     off_t res;
 
-    res = lseek (src->fd, offset, SEEK_SET);
-    if (G_UNLIKELY (res == (off_t) - 1 || res != offset))
+    res = fseeko (src->fp, offset, SEEK_SET);
+    if (G_UNLIKELY (res != 0))
       goto seek_failed;
 
     src->read_position = offset;
@@ -341,7 +338,7 @@ gst_file_src_fill (GstBaseSrc * basesrc, guint64 offset, guint length,
     GST_LOG_OBJECT (src, "Reading %d bytes at offset 0x%" G_GINT64_MODIFIER "x",
         to_read, offset + bytes_read);
     errno = 0;
-    ret = read (src->fd, data + bytes_read, to_read);
+    ret = fread (data + bytes_read, 1, to_read, src->fp);
     if (G_UNLIKELY (ret < 0)) {
       if (errno == EAGAIN || errno == EINTR)
         continue;
@@ -447,38 +444,39 @@ gst_file_src_get_size (GstBaseSrc * basesrc, guint64 * size)
      * succeed, and wrongly say our length is zero. */
     return FALSE;
   }
+
+  if (!src->fp)
+    return FALSE;
+
 #ifdef G_OS_WIN32
   {
-    HANDLE h = gst_file_src_win32_get_osfhandle (src->fd);
+    HANDLE h = (HANDLE) _get_osfhandle (_fileno (src->fp));
     LARGE_INTEGER file_size;
 
     if (h == INVALID_HANDLE_VALUE)
-      goto could_not_stat;
+      return FALSE;
 
-    if (!GetFileSizeEx (h, &file_size)) {
-      goto could_not_stat;
-    }
+    if (!GetFileSizeEx (h, &file_size))
+      return FALSE;
 
     *size = file_size.QuadPart;
   }
 #else
   {
     struct_stat stat_results;
+    int fd = fileno (src->fp);
 
-    if (fstat (src->fd, &stat_results) < 0)
-      goto could_not_stat;
+    if (fd == -1)
+      return FALSE;
+
+    if (fstat (fileno (src->fp), &stat_results) < 0)
+      return FALSE;
 
     *size = stat_results.st_size;
   }
 #endif
 
   return TRUE;
-
-  /* ERROR */
-could_not_stat:
-  {
-    return FALSE;
-  }
 }
 
 /* open the file, necessary to go to READY state */
@@ -486,10 +484,6 @@ static gboolean
 gst_file_src_start (GstBaseSrc * basesrc)
 {
   GstFileSrc *src = GST_FILE_SRC (basesrc);
-  int flags = O_RDONLY | O_BINARY;
-#if defined (__BIONIC__)
-  flags |= O_LARGEFILE;
-#endif
 
   if (src->filename == NULL || src->filename[0] == '\0')
     goto no_filename;
@@ -497,14 +491,14 @@ gst_file_src_start (GstBaseSrc * basesrc)
   GST_INFO_OBJECT (src, "opening file %s", src->filename);
 
   /* open the file */
-  src->fd = g_open (src->filename, flags, 0);
+  src->fp = fopen (src->filename, "rb");
 
-  if (src->fd < 0)
+  if (!src->fp)
     goto open_failed;
 
 #ifdef G_OS_WIN32
   {
-    HANDLE h = gst_file_src_win32_get_osfhandle (src->fd);
+    HANDLE h = (HANDLE) _get_osfhandle (_fileno (src->fp));
     FILE_STANDARD_INFO file_info;
 
     if (h == INVALID_HANDLE_VALUE)
@@ -525,7 +519,7 @@ gst_file_src_start (GstBaseSrc * basesrc)
     struct_stat stat_results;
 
     /* check if it is a regular file, otherwise bail out */
-    if (fstat (src->fd, &stat_results) < 0)
+    if (fstat (fileno (src->fp), &stat_results) < 0)
       goto no_stat;
 
     if (S_ISDIR (stat_results.st_mode))
@@ -544,14 +538,14 @@ gst_file_src_start (GstBaseSrc * basesrc)
 
   /* We need to check if the underlying file is seekable. */
   {
-    off_t res = lseek (src->fd, 0, SEEK_END);
+    off_t res = fseeko (src->fp, 0, SEEK_END);
 
     if (res == (off_t) - 1) {
       GST_LOG_OBJECT (src, "disabling seeking, lseek failed: %s",
           g_strerror (errno));
       src->seekable = FALSE;
     } else {
-      res = lseek (src->fd, 0, SEEK_SET);
+      res = fseeko (src->fp, 0, SEEK_SET);
 
       if (res == (off_t) - 1) {
         /* We really don't like not being able to go back to 0 */
@@ -621,7 +615,7 @@ lseek_wonky:
     goto error_close;
   }
 error_close:
-  close (src->fd);
+  g_clear_pointer (&src->fp, fclose);
 error_exit:
   return FALSE;
 }
@@ -632,11 +626,8 @@ gst_file_src_stop (GstBaseSrc * basesrc)
 {
   GstFileSrc *src = GST_FILE_SRC (basesrc);
 
-  /* close the file */
-  g_close (src->fd, NULL);
+  g_clear_pointer (&src->fp, fclose);
 
-  /* zero out a lot of our state */
-  src->fd = 0;
   src->is_regular = FALSE;
 
   return TRUE;
