c8874164dd39657571d1c48f5c83b7ce5c878daf
Seungha Yang <seungha@centricular.com>
2023-06-21 23:10:07 +0900

    decklink2src: Add "desync-threshold" and "no-signal-threshold" properties
    Once large A/V desync or no-signal frames are detected, src will
    automatically restart streaming

--- subprojects/gst-plugins-bad/sys/decklink2/gstdecklink2input.h
+++ subprojects/gst-plugins-bad/sys/decklink2/gstdecklink2input.h
@@ -74,7 +74,9 @@ void                gst_decklink2_input_set_flushing (GstDeckLink2Input * input,
 
 GstFlowReturn       gst_decklink2_input_get_data (GstDeckLink2Input * input,
                                                   GstBuffer ** buffer,
-                                                  GstCaps ** caps);
+                                                  GstCaps ** caps,
+                                                  GstClockTimeDiff * av_sync,
+                                                  guint * no_signal_count);
 
 gboolean            gst_decklink2_input_has_signal (GstDeckLink2Input * input);
 
